c030ef7b20cafe7099749e45f88480bded8334cb
erikdiessel <erik.diessel@gmx.de>
2014-03-02 16:21:21 +0000

    Added spec for Generator-link

--- spec/IntegrationSpec.js
+++ spec/IntegrationSpec.js
@@ -5,15 +5,22 @@ var browser = new webdriver.Builder().
     usingServer('http://localhost:4444').
     build();
 
-jasmine.getEnv().defaultTimeoutInterval = 10000;
+jasmine.getEnv().defaultTimeoutInterval = 3000;
 
 describe("The starting page", function() {
     beforeEach(function() {
         browser.get("http://localhost:8080");
     });
-    it("Has the title 'Safify'", function(done) {
+    it("has the title 'Safify'", function(done) {
         browser.getTitle().then(function(title) {
             expect(title).toEqual("Safify");
+            done();
+        });
+    });
+    it("has a link to the Generator", function(done) {
+        el = browser.findElement(webdriver.By.partialLinkText('Generator'));
+        el.getAttribute("href").then(function(href) {
+            expect(href).toMatch(/.+\/#generator/);
             done();
         });
     });
